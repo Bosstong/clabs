5800c2004861298d0361d47a562416e969de5757
Bosstong <240136159@qq.com>
2019-10-25 08:42:38 -0700

    added a bc.c file

---
+++ bc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+    int count = 1;
+    int number = 0;
+    int number2 = 0;
+    printf("please input the number\n");
+    scanf("%5d", &number2);
+    while(number2 !=1)
+    {
+        number = number2%2;
+        number2 = number2/2;
+        if(number ==1)
+        count++;
+    }
+    printf("the amount is:%d\n", count);
+}

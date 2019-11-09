9a7af56516c5827500620e3594face620c680f9b
Bosstong <240136159@qq.com>
2019-11-09 05:45:00 -0800

    added a lab3.c file

--- lab3.c
+++ lab3.c
@@ -3,9 +3,8 @@
 int strindex (char s[], char t[])
 {
     int i,j,k,pos=0;
-    for(i = 0;s[i] !='\0'; i++)
-    {
-        for(j = i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
+    for(i = 0;s[i] !='\0'; ++i){
+        for(j = i-1,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
         if(t[k]=='\0'){ 
         pos = i;
     }

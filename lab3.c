a14d9b0c09841e62eeb39eeec23638f1f4796654
Bosstong <240136159@qq.com>
2019-11-09 18:27:40 -0800

    added a lab3.c file

--- lab3.c
+++ lab3.c
@@ -2,15 +2,16 @@
 #include<string.h>
 int strindex (char s[], char t[])
 {
-    int i,j,k,pos=0;
+    int i,j,k,a,pos=0;
     for(i = 0;s[i] !='\0'; i++){
         for(j = i-1,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
         if(k>0&&t[k]=='\0'){
         pos = i;
+        a=k;
     }
     }
     if(pos >0)
-    return pos+1;
+    return (pos+a-2);
     return -1;
 }
 main(){

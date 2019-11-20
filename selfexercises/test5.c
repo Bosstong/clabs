eafcd7eb662f2ef0177915d732765b9ab203b155
Bosstong <240136159@qq.com>
2019-11-20 04:42:53 -0800

    added a test5.c file

---
+++ selfexercises/test5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+unsigned long long int a = 1;
+int count = 0;
+while(a!=0){
+    a=a<<1;
+    count++;
+}
+printf("the max is %llu,the min is %llu the sizeof if %d\n",a-1,a+1,count);   
+}
